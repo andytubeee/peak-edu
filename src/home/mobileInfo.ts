453b8b4234f30be3c73ad63f761663a42a0be253
Luke-zhang-04 <luke.zhang2004dev@gmail.com>
2020-11-09 11:22:37 -0500

    update LICENSE
    Signed-off-by: Luke-zhang-04 <luke.zhang2004dev@gmail.com>

--- src/home/mobileInfo.ts
+++ src/home/mobileInfo.ts
@@ -6,19 +6,8 @@
  * @author Luke Zhang
  * https://luke-zhang-04.github.io/
  * 
- * @license GPL-3.0
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @license Modified-BSD-3-Clause
+ * @see {@link https://github.com/Luke-zhang-04/peak-edu/blob/master/LICENSE}
  */
 import DeStagnate, {createElement} from "destagnate"
 import {default as infoData} from "./infoData.json"
